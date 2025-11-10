9093d7afd2dafa3aebc727a17a4221dfa8190106
demigod <213689604+defxq@users.noreply.github.com>
2025-11-10 15:27:07 +0630

    testing with no cors

--- src/configs/corsOption.js
+++ src/configs/corsOption.js
@@ -2,8 +2,7 @@ import allowedOrigins from "./allowedOrigins.js";
 
 const corsOption = {
     origin: (origin, callback) => {
-        console.log("CORS origin received:", origin);
-        if (allowedOrigins.includes(origin)) {
+        if (allowedOrigins.includes(origin) || !cors) {
             callback(null, true);
         } else {
             callback(new Error("Error: Blocked by cors"));
